23f708967bfdad16275ad74d34086c8300b28660
danielAwale <danielmadhukarawale@gmail.com>
2022-11-14 07:05:52 -0800

    contain the started api code for the imdb api from rapid api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import './App.css';
 
 function App() {
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'c31c0f80f4mshbb6b108a22becd7p1ccc72jsn990e3c358a6d',
+      'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+    }
+  };
+
+  fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
   return (
     <div className="App">
     </div>
