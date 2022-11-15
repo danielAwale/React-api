7377cba36039ca88db169ac7b6b1606e1372bdb8
danielAwale <danielmadhukarawale@gmail.com>
2022-11-15 06:53:42 -0800

    contain the imagge url in the img source that will render

--- src/App.js
+++ src/App.js
@@ -5,41 +5,42 @@ function App() {
   const [endPoint, setEndPoint] = useState('')
   const [container, setContainer] = useState([])
 
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'c31c0f80f4mshbb6b108a22becd7p1ccc72jsn990e3c358a6d',
-      'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-    }
-  };
-
   useEffect(() => {
-    fetchMe()
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'c31c0f80f4mshbb6b108a22becd7p1ccc72jsn990e3c358a6d',
+        'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+      }
+    };
+    fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=+${endPoint}`, options)
+      .then(response => { return response.json()})
+      .then(data => setContainer(data.d))
+      .catch(err => console.error(err));
   },[endPoint])
 
 
-  const fetchMe = () => {
-  fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=+${endPoint}`, options)
-    .then(response => { return response.json()})
-    .then(data => setContainer(data.d))
-    .catch(err => console.error(err));
-  }
-
     const submitHandler = (e) => {
       e.preventDefault();
+    }
 
+    const changeEndpoing = (e) => {
+      setEndPoint(e.target.value)
     }
 
   return (
     <div className="App">
       <form onSubmit={submitHandler}>
-        <input type="" value={endPoint} onChange={(e) => setEndPoint(e.target.value)}/>
+        <input type="text" value={endPoint} onChange={changeEndpoing}/>
         <button type='submit'>submit</button>
       </form>
 
       {container.map((item) => {
         return (
-          <p>{item.l}</p>
+          <div>
+            <img src={item.i.imageUrl} alt="" />
+            <p>{item.l}</p>
+          </div>
         )
       })}
     </div>
